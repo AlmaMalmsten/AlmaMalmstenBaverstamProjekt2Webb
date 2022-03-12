19d730eef8144e290d15a77346c73cb462d12c9d
alma <alma.malmstenbaverstam@gmail.com>
2022-03-12 19:36:54 +0100

    its shit but whatever

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
-let player1 = "ex";
-let player2 = "square";
+let cell0 = document.querySelector(".HowTo110");
+let cell1 = document.querySelector(".HowTo111");
+let cell2 = document.querySelector(".HowTo112");
+let cell3 = document.querySelector(".HowTo113");
+let cell4 = document.querySelector(".HowTo114");
+let cell5 = document.querySelector(".HowTo115");
+let cell6 = document.querySelector(".HowTo116");
+let cell7 = document.querySelector(".HowTo117");
+let cell8 = document.querySelector(".HowTo118");
 
-let individualElements = document.querySelectorAll('[data-111]');
-let wholeElement = document.getElementsByClassName('HowTo11');
-let buttonElement = document.getElementsByClassName('HowTo1Button');
+let button = document.querySelector(".button1")
+let body = document.querySelector(".HowTo")
 
 
+cell0.addEventListener("click", plant0R);
+cell1.addEventListener("click", plant1R);
+cell2.addEventListener("click", plant2R);
+cell3.addEventListener("click", plant3R);
+cell4.addEventListener("click", plant4R);
+cell5.addEventListener("click", plant5R);
+cell6.addEventListener("click", plant6R);
+cell7.addEventListener("click", plant7R);
+cell8.addEventListener("click", plant8R);
 
-function start(){
-    let player1Turn = false;
-    individualElements.forEach(individual => {
-        individual.classList.remove(player1)
-        individual.classList.remove(player2)
-        individual.removeEventListener('click')
-    })
+button.addEventListener("click", buttonShit);
+
+function buttonShit() {
+    body.style.backgroundColor = "#df2d4d";
+    plant0R()
+    plant1R()
+    plant2R()
+    plant3R()
+    plant4R()
+    plant5R()
+    plant6R()
+    plant7R()
+    plant8R()
 }
 
-
-/*let Winning = [
-    [0, 1, 2]
-    [3, 4, 5]
-    [6, 7, 8]
-    [1, 4, 8]
-    [2, 4, 6]
-    [0, 3, 6]
-    [1, 4, 7]
-    [2, 5, 8]
-]*/
+function plant0R() {
+  cell0.style.backgroundColor = "#df2d4d";
+}
+function plant1R() {
+  cell1.style.backgroundColor = "#df2d4d";
+}
+function plant2R() {
+  cell2.style.backgroundColor = "#df2d4d";
+}
+function plant3R() {
+  cell3.style.backgroundColor = "#df2d4d";
+}
+function plant4R() {
+  cell4.style.backgroundColor = "#df2d4d";
+}
+function plant5R() {
+  cell5.style.backgroundColor = "#df2d4d";
+}
+function plant6R() {
+  cell6.style.backgroundColor = "#df2d4d";
+}
+function plant7R() {
+  cell7.style.backgroundColor = "#df2d4d";
+}
+function plant8R() {
+  cell8.style.backgroundColor = "#df2d4d";
+}
